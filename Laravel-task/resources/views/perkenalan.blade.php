d761dc9da880702e68cceb1ebe8e72c19df6f542
Febry Billiyagi <89958256+billiyagi@users.noreply.github.com>
2024-05-26 01:55:14 +0700

    Upload Task: Perkenalan

---
+++ Laravel-task/resources/views/perkenalan.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+        <style>
+            .btn-black {
+                background-color: #000;
+                color: #fff;
+            }
+            .btn-black:hover {
+                background-color: #fff;
+                color: #000;
+            }
+        </style>
+</head>
+
+<body style="background-color: #FFA62F">
+
+    <main>
+        <div class="container">
+            <div class="row vh-100 justify-content-center align-items-center">
+                <div class="col-3 position-relative">
+                    <div class="justify-content-end d-flex w-100">
+                        <img src="/img/profile.png" class="img-fluid" alt="Febry Billiyagi Karsidi" data-aos="fade-down" data-aos-duration="2000">
+                    </div>
+                </div>
+                <div class="col-9 px-5">
+                    <h1 style="font-size: 5em" data-aos="fade-up" data-aos-duration="2200">Hi,</h1>
+                    <p style="font-size: 2em" data-aos="fade-up" data-aos-duration="2500">Saya Febry Billiyagi Karsidi, seorang mahasiswa dari STT Terpadu Nurul Fikri yang sedang menjalani kuliah disemester 4 dengan jurusan Teknik Informatika, saat ini fokus saya adalah dalam
+                    Pengembangan Software dan Artificial intelligence.</p>
+                    <div class="pt-3">
+                        <a href="http://github/billiyagi" class="btn btn-lg btn-black" data-aos="fade-up" data-aos-duration="2400"><i class="bi bi-github"></i> Github</a>
+                        <a href="http://linkedin/in/billiyagi" class="btn btn-lg btn-black" data-aos="fade-up" data-aos-duration="2800"><i class="bi bi-linkedin"></i> Linkedin</a>
+                        <a href="http://billiyagi.space?from=tugasjarvis" class="btn btn-lg btn-black" data-aos="fade-up" data-aos-duration="3000"><i class="bi bi-globe"></i> Website</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+    <script>
+        AOS.init();
+    </script>
+</body>
+
+
+
+</html>
